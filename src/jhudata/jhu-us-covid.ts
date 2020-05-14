5c71f0c870dbb5ce6ca7695c8f1bf6214c5252f9
Rolfe Schmidt <rolfeschmidt@gmail.com>
2020-05-13 19:54:59 -0500

    load jhu us death data

--- src/jhudata/jhu-us-covid.ts
+++ src/jhudata/jhu-us-covid.ts
@@ -5,16 +5,21 @@
  * International License  (http://creativecommons.org/licenses/by-nc-sa/4.0/)
  */
 import { Dataset, Member, Measure, Level, TimeSeries, ParentMember, linkLevels } from '../types/dataset'
-const DATA_URL =
+const CASES_URL =
     'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+const DEATHS_URL =
+    'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_US.csv'
 
 export class JHUCovid19USDataset implements Dataset {
     levels: Level[] = []
     measures: Measure[] = [{ name: 'confirmed' }, { name: 'new' }]
+    population: { [key: string]: number } = {}
     timePoints: Date[] = []
 
     private _confirmed: { [m: string]: number[] } = {}
     private _newCases: { [m: string]: number[] } = {}
+    private _deaths: { [m: string]: number[] } = {}
+    private _newDeaths: { [m: string]: number[] } = {}
     private _members: { [id: string]: Member } = {}
 
     private _loaded: Promise<boolean>
@@ -32,14 +37,20 @@ export class JHUCovid19USDataset implements Dataset {
     }
 
     async load(): Promise<boolean> {
-        const data = await fetch(DATA_URL).then((response: Response) => response.text())
-        const lines = data.split('\n')
-        this._readHeader(lines[0])
-        for (let i = 1; i < lines.length; ++i) {
-            this._readLine(lines[i])
+        const caseData = await fetch(CASES_URL).then((response: Response) => response.text())
+        const clines = caseData.split('\n')
+        this._readHeader(clines[0])
+        for (let i = 1; i < clines.length; ++i) {
+            this._readLine(clines[i], 'confirmed')
         }
         console.log(`data keys`, Object.keys(this._confirmed))
 
+        const deathData = await fetch(DEATHS_URL).then((response: Response) => response.text())
+        const dlines = deathData.split('\n')
+        // this._readHeader(dlines[0])
+        for (let i = 1; i < dlines.length; ++i) {
+            this._readLine(dlines[i], 'deaths')
+        }
         return true
     }
     get members(): { [k: string]: Member } {
@@ -56,7 +67,7 @@ export class JHUCovid19USDataset implements Dataset {
         this.timePoints = dates.map((str: string) => new Date(str))
     }
 
-    private _readLine(line: string): void {
+    private _readLine(line: string, measure: string): void {
         const parts = line.split(/,".*",/)
         if (parts.length !== 2) {
             return
@@ -64,19 +75,23 @@ export class JHUCovid19USDataset implements Dataset {
         const [uid, iso2, iso3, code3, fips, county, state, country] = parts[0].split(',')
         // console.log({ uid, iso2, iso3, code3, fips, county, state, country, key: `${country}.${state}.${county}` })
 
-        const values = parts[1].split(',').map((s: string) => parseInt(s, 10))
+        let values = parts[1].split(',').map((s: string) => parseInt(s, 10))
 
         const countryKey = country
         const stateKey = `${country}.${state}`
         const countyKey = `${country}.${state}.${county}`
         let countryMember = this._members[countryKey] as ParentMember
         let stateMember = this._members[stateKey] as ParentMember
+        let leafMember = this._members[countyKey]
         if (!countryMember) {
             countryMember = new ParentMember(country, this.countryLevel)
             this._members[countryKey] = countryMember
             this.levels[0].members.push(countryMember)
             this._confirmed[countryMember.id] = new Array(values.length).fill(0)
             this._newCases[countryMember.id] = new Array(values.length).fill(0)
+            this._deaths[countryMember.id] = new Array(values.length).fill(0)
+            this._newDeaths[countryMember.id] = new Array(values.length).fill(0)
+            this.population[countryMember.id] = 0
         }
         if (!stateMember) {
             stateMember = new ParentMember(state, this.stateLevel, countryMember)
@@ -85,20 +100,42 @@ export class JHUCovid19USDataset implements Dataset {
             this.levels[1].members.push(stateMember)
             this._confirmed[stateMember.id] = new Array(values.length).fill(0)
             this._newCases[stateMember.id] = new Array(values.length).fill(0)
+            this._deaths[stateMember.id] = new Array(values.length).fill(0)
+            this._newDeaths[stateMember.id] = new Array(values.length).fill(0)
+            this.population[stateMember.id] = 0
         }
-        const leafMember = new Member(county, this.countyLevel, stateMember)
-        this._members[countyKey] = leafMember
-        this.levels[2].members.push(leafMember)
-        stateMember.children.push(leafMember)
-        this._newCases[leafMember.id] = new Array(values.length)
+        if (!leafMember) {
+            leafMember = new Member(county, this.countyLevel, stateMember)
+            this._members[countyKey] = leafMember
+            this.levels[2].members.push(leafMember)
+            stateMember.children.push(leafMember)
+        }
+        if (measure === 'confirmed') {
+            this._newCases[leafMember.id] = new Array(values.length)
 
-        this._confirmed[leafMember.id] = values
-        for (let i = 0; i < values.length; ++i) {
-            this._confirmed[countryMember.id][i] += values[i]
-            this._confirmed[stateMember.id][i] += values[i]
-            this._newCases[countryMember.id][i] += values[i] - (values[i - 1] || 0)
-            this._newCases[stateMember.id][i] += values[i] - (values[i - 1] || 0)
-            this._newCases[leafMember.id][i] = values[i] - (values[i - 1] || 0)
+            this._confirmed[leafMember.id] = values
+            for (let i = 0; i < values.length; ++i) {
+                this._confirmed[countryMember.id][i] += values[i]
+                this._confirmed[stateMember.id][i] += values[i]
+                this._newCases[countryMember.id][i] += values[i] - (values[i - 1] || 0)
+                this._newCases[stateMember.id][i] += values[i] - (values[i - 1] || 0)
+                this._newCases[leafMember.id][i] = values[i] - (values[i - 1] || 0)
+            }
+        } else if (measure === 'deaths') {
+            this.population[leafMember.id] = values[0]
+            this.population[countryMember.id] += values[0]
+            this.population[stateMember.id] += values[0]
+            values = values.slice(1)
+            this._newDeaths[leafMember.id] = new Array(values.length)
+
+            this._deaths[leafMember.id] = values
+            for (let i = 0; i < values.length; ++i) {
+                this._deaths[countryMember.id][i] += values[i]
+                this._deaths[stateMember.id][i] += values[i]
+                this._newDeaths[countryMember.id][i] += values[i] - (values[i - 1] || 0)
+                this._newDeaths[stateMember.id][i] += values[i] - (values[i - 1] || 0)
+                this._newDeaths[leafMember.id][i] = values[i] - (values[i - 1] || 0)
+            }
         }
     }
 
@@ -108,6 +145,8 @@ export class JHUCovid19USDataset implements Dataset {
         const measures = {
             confirmed: this._confirmed,
             newCases: this._newCases,
+            deaths: this._deaths,
+            newDeaths: this._newDeaths,
         } as { [k: string]: any }
         return {
             name: `${member.id}.${measure.name}`,
